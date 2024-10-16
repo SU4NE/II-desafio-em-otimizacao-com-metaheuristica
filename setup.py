88f7bfbfe6d9c59ced26ee7a44882cb2e0306c23
Portin <joaovictorpf719@gmail.com>
2024-10-16 05:01:00 -0300

    Bug fix

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ setup(
         "tqdm>=4.66.5",
         "wheel>=0.44.0",
         'tabu_structure @ https://github.com/SU4NE/II-desafio-em-otimizacao-com-metaheuristica/releases/download/tabu_structure/tabu_structure-0.1.0-cp38-none-win_amd64.whl; platform_system=="Windows"',
-        'tabu_structure @ https://github.com/SU4NE/II-desafio-em-otimizacao-com-metaheuristica/releases/download/tabu_structure/tabu_structure-0.1.0-cp38-cp38-linux_x86_64.whl; platform_system=="Linux"'
+        'tabu_structure @ https://github.com/SU4NE/II-desafio-em-otimizacao-com-metaheuristica/releases/download/tabu_structure/tabu_structure-0.1.0-cp38-cp38-linux_x86_64.whl; platform_system=="Linux"',
     ],
 )
