58c405b2def47d360086846c3b5c3ac8c4e343a8
Portin <joaovictorpf719@gmail.com>
2024-10-16 05:02:16 -0300

    Setup bug fix

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ setup(
     description='A Python package for solving the Bin Packing Problem using various heuristic algorithms, including Particle Swarm Optimization, Gravitational Search Algorithm, and Improved Whale Optimization Algorithm. This package provides an easy-to-use interface for implementing and comparing different metaheuristics to find optimal packing solutions efficiently.',
     install_requires=[
         'tabulate>=0.9.0',
-        "numpy>=1.24.4",
-        "tqdm>=4.66.5",
-        "wheel>=0.44.0",
+        'numpy>=1.24.4',
+        'tqdm>=4.66.5',
+        'wheel>=0.44.0',
         'tabu_structure @ https://github.com/SU4NE/II-desafio-em-otimizacao-com-metaheuristica/releases/download/tabu_structure/tabu_structure-0.1.0-cp38-none-win_amd64.whl; platform_system=="Windows"',
-        'tabu_structure @ https://github.com/SU4NE/II-desafio-em-otimizacao-com-metaheuristica/releases/download/tabu_structure/tabu_structure-0.1.0-cp38-cp38-linux_x86_64.whl; platform_system=="Linux"',
+        'tabu_structure @ https://github.com/SU4NE/II-desafio-em-otimizacao-com-metaheuristica/releases/download/tabu_structure/tabu_structure-0.1.0-cp38-cp38-linux_x86_64.whl; platform_system=="Linux"'
     ],
 )
